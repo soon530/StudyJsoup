14023b8713e8e0efcdff3094dc344c5236940aed
soon530 <soon530@gmail.com>
2014-04-07 17:39:44 +0800

    分析「下一頁」，如果有則繼續往下抓，如果沒有就停止
    OPEN - #2: 研究一下如何將保母網搜尋出來的的保母表列存到local 
    https://github.com/soon530/jsoup_study/issues/issue/2

--- src/tasker/CwiswebTest.java
+++ src/tasker/CwiswebTest.java
@@ -3,9 +3,12 @@ package tasker;
 import static org.junit.Assert.fail;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.io.Writer;
+import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -56,6 +59,7 @@ public class CwiswebTest {
 
 	/**
 	 * 直接撈全部的話很慢
+	 * 
 	 * @throws IOException
 	 */
 	@Test
@@ -73,6 +77,7 @@ public class CwiswebTest {
 
 	/**
 	 * 加上縣市的話快很多
+	 * 
 	 * @throws IOException
 	 */
 	@Test
@@ -87,7 +92,7 @@ public class CwiswebTest {
 		System.out.println("title" + title);
 		System.out.println("all" + doc.body());
 	}
-	
+
 	@Test
 	public void loadBabySitterDetailPageFromUrl() throws IOException {
 		String sn = "E222138751";
@@ -99,18 +104,76 @@ public class CwiswebTest {
 	}
 
 	@Test
-	public void loadBabySitterDetailPageFromUrlAndSaveToLocal() throws IOException {
+	public void loadBabySitterDetailPageFromUrlAndSaveToLocal()
+			throws IOException {
 		String sn = "E222138751";
 		String url = "http://cwisweb.sfaa.gov.tw/04nanny/03view.jsp";
 		Document doc = Jsoup.connect(url).data("sn", sn).timeout(3000).post();
 		String html = doc.html();
-		
-		//String charset = Jsoup.connect(url).response().charset();
+
+		// String charset = Jsoup.connect(url).response().charset();
 		// ...
-		Writer writer = new PrintWriter("D:\\workspace\\workspace_web_jsoup\\jsoup\\tmp\\03view"+sn+".htm", "UTF-8");
+		Writer writer = new PrintWriter(
+				"D:\\workspace\\workspace_web_jsoup\\jsoup\\tmp\\03view" + sn
+						+ ".htm", "UTF-8");
 		writer.write(html);
 		writer.close();
 	}
 
+	/**
+	 * http://cwisweb.sfaa.gov.tw/04nanny/03list.jsp?MEM_REGION=6400000000&
+	 * MEM_CITY=6400001000&d1=&imageField.x=48&imageField.y=18
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void loadAllBabySitterWebListPageFromUrlAndSaveToLocal()
+			throws IOException {
+		String MEM_REGION = "6400000000";
+		String MEM_CITY = "6400001000";
+		ArrayList<Document> docs = new ArrayList<Document>();
+		Document doc = null;
+		int page = -1;
+		int offset = 0;
 
+		do {
+			page++;
+			offset = page * 10;
+			String url = "http://cwisweb.sfaa.gov.tw/04nanny/03list.jsp?"
+					+ "MEM_REGION=" + MEM_REGION + "&MEM_CITY=" + MEM_CITY
+					+ "&d1=&imageField.x=44&imageField.y=20&offset=" + offset;
+
+			doc = Jsoup.connect(url).timeout(10000).get();
+			System.out.println("url=" + url + ",page=" + page);
+			System.out.println(hasNextPage(doc));
+			docs.add(doc);
+			
+		} while (hasNextPage(doc));
+		
+		writeDocsToHtmlInLocalPath(docs);
+	}
+
+	private void writeDocsToHtmlInLocalPath(ArrayList<Document> docs)
+			throws FileNotFoundException, UnsupportedEncodingException,
+			IOException {
+		int count = 1;
+		String localPath = null;
+		Writer writer = null;
+		for (Document babysitterListDoc : docs) {
+			localPath="D:\\workspace\\workspace_web_jsoup\\jsoup\\tmp\\kao" + count + ".htm";
+			writer = new PrintWriter(localPath, "UTF-8");
+			writer.write(babysitterListDoc.html());
+			writer.close();
+			count++;
+		}
+	}
+
+	private boolean hasNextPage(Document doc) {
+		Element link = doc.select("div.page").select("a").last();
+
+		if (link.text().equals("下一頁"))
+			return true;
+		else
+			return false;
+	}
 }
